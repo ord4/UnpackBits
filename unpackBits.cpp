b8c6ed2c7e2f58488879e02059e947075bacde8e
ord4 <ord4@zips.uakron.edu>
2017-04-26 19:46:35 -0400

    Ready for submission

--- unpackBits.cpp
+++ unpackBits.cpp
@@ -11,6 +11,11 @@
 void unpackCharacters(char[], unsigned);
 void displayBits(unsigned);
 
+const unsigned MASK1 = 4278190080;
+const unsigned MASK2 = 16711680;
+const unsigned MASK3 = 65280;
+const unsigned MASK4 = 255;
+
 int main() {
    char characters[4];
    unsigned packed{1633903975}; // a, c, e, g
@@ -37,7 +42,10 @@ int main() {
 // take apart the characters
 void unpackCharacters(char characters[], unsigned pack)
 {
-    
+	characters[0] = (pack & MASK1) >> 24;
+	characters[1] = (pack & MASK2) >> 16;
+	characters[2] = (pack & MASK3) >> 8;
+	characters[3] = (pack & MASK4);
 }
 
 // display the bits of value
